0203868c75b3bd1273fde62b432ec38e90e8da81
Bailee Dastugue <bailee.dast@gmail.com>
2020-03-30 10:25:25 -0500

    links to both repo + deployed sites update on click

--- assets/javascript/page.js
+++ assets/javascript/page.js
@@ -2,48 +2,48 @@ var wordGuess = {
     id: "wordGuess",
     largeImg: "assets/images/compHM.png",
     title: "Hang Man Game",
-    gitHub: "",
-    deployed: ""
+    gitHub: "https://github.com/baileedastugue/Word-Guess-Game",
+    deployed: "https://baileedastugue.github.io/Word-Guess-Game/"
 }
 
 var trivia = {
     id: "trivia",
     largeImg: "assets/images/compBJ.png",
     title: "Billy Joel Trivia Game",
-    gitHub: "",
-    deployed: ""
+    gitHub: "https://github.com/baileedastugue/TriviaGame",
+    deployed: "https://baileedastugue.github.io/TriviaGame/"
 }
 
 var rpgGame = {
     id: "rpg",
     largeImg: "assets/images/compRPG.png",
     title: "Role Play Game",
-    gitHub: "",
-    deployed: ""
+    gitHub: "https://github.com/baileedastugue/RPG-game",
+    deployed: "https://baileedastugue.github.io/RPG-game/"
 }
 
 var rockPaperScissors = {
     id: "rps",
     largeImg: "assets/images/compRPS.png",
     title: "Rock Paper Scissors Game",
-    gitHub: "",
-    deployed: ""
+    gitHub: "https://github.com/baileedastugue/RPS-Multiplayer",
+    deployed: "https://baileedastugue.github.io/RPS-Multiplayer/"
 }
 
 var giphyAPP = {
     id: "giphy",
     largeImg: "assets/images/compGiphy.png",
     title: "Giphy API Application",
-    gitHub: "",
-    deployed: ""
+    gitHub: "https://github.com/baileedastugue/GiphyApp",
+    deployed: "https://baileedastugue.github.io/GiphyApp/"
 }
 
 var marvel = {
     id: "project1",
     largeImg: "assets/images/compMarvel.png",
     title: "Marvel + OMDb API Group Project",
-    gitHub: "",
-    deployed: ""
+    gitHub: "https://github.com/Kianimal/Marvel_Comics_Central",
+    deployed: "https://Kianimal.github.io/Marvel_Comics_Central"
 }
 
 var optionsArray = [wordGuess, trivia, rpgGame, rockPaperScissors, giphyAPP, marvel]
@@ -56,6 +56,8 @@ var currentPick;
          if (optionsArray[i].id === currentPick) {
              $(".card-img-top").attr("src", optionsArray[i].largeImg);
              $(".card-title").text(optionsArray[i].title);
+             $("#git").attr("href", optionsArray[i].gitHub);
+             $("#deployed").attr("href", optionsArray[i].deployed);
          }
      }
  })
